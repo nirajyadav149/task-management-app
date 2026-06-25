1756edd6b798331b46debbaab3e433666149ebb2
nirajyadav149 <nirajyadav36149@gmail.com>
2026-06-25 14:26:26 +0530

    Set up API routes for tasks and authentication

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/tasks", require("./routes/taskRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
+
 app.get("/", (req, res) => {
   res.send("Task Management API Running...");
 });
